e865db3fbc8768db62dd45fa863e6bd8a76adce2
AnugrahPutra_BCN <anugrahputra@gmail.com>
2025-06-09 17:52:02 +0700

    melakukan perulangan dan menutup file

--- iofile.cpp
+++ iofile.cpp
@@ -34,4 +34,18 @@ int main()
     // membuka file
     infile.open("contohfile.txt");
 
-   
+    cout << endl
+         << ">= Membuka dan membaca file " << endl;
+        // jika file ada maka
+    if (infile.is_open())
+    {
+        // melakukan perulangan setiap baris
+        while(getline(infile, baris))
+        {
+            // dan tampilkan di sini
+            cout << baris << '\n';
+        }
+        // tutup file tersebut setelah selesai 
+        infile.close();
+    }
+ 
