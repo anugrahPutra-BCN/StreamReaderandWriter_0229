a3f1d24fa4e5f70a3c8e84015bd433a755f6dbfd
AnugrahPutra_BCN <anugrahputra@gmail.com>
2025-06-09 17:41:29 +0700

    membuat obyek output file stream dan membuka file

--- iofile.cpp
+++ iofile.cpp
@@ -7,4 +7,11 @@ int main()
 {
     string baris;
 
+    // membuat obyek output file stream dalam mode menulis.
+    ofstream outfile;
+    // membuka file
+    outfile.open("contohfile.txt");
+    
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
     
