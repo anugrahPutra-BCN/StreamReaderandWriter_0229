b0918dface1bc9586d3b5a3ae91d86981119e87d
AnugrahPutra_BCN <anugrahputra@gmail.com>
2025-06-09 19:07:19 +0700

    menambahkan catch

--- manuaLEception.cpp
+++ manuaLEception.cpp
@@ -9,4 +9,17 @@ int main()
         throw("halo"); // melemparkan sebuah intiger maka
         cout << "Pertanyaan tidak akan dieksekusi" << endl;
     }
+    catch(int a)
+    {
+        // blok ini akan dieksekusi
+        cout << "Pengecualian akan dieksekusi" << endl;
+    }
+    catch (...)
+    {
+        /*jika selain integer maka blok ini akan dieksekusi*/
+        cout << "default Pengecualian dieksekusi" << endl;
+    }
+
+    return 0;
     
+}
