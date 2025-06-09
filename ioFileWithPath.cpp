db98f7d91f460040fe0d3c104b907a4223a836f3
AnugrahPutra_BCN <anugrahputra@gmail.com>
2025-06-09 19:01:29 +0700

    menutup filenya

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -30,4 +30,7 @@ int main()
         // menulis dan memasukan nilai dari 'baris' ke dalam file
         outfile << baris << endl;
     }
- 
+    //selesai dalam menulis sekarang tutup filenya
+    outfile.close();
+
+   
