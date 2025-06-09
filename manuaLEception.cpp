c31e8c0a7ef51b06e059796a46c3accda6a1f5e5
AnugrahPutra_BCN <anugrahputra@gmail.com>
2025-06-09 19:06:50 +0700

    menambahkan try

--- manuaLEception.cpp
+++ manuaLEception.cpp
@@ -2,3 +2,11 @@
 using namespace std;
 
 int main()
+{
+    try
+    {
+        cout << "Selamat Belajar di Prodi TI UMY" << endl;
+        throw("halo"); // melemparkan sebuah intiger maka
+        cout << "Pertanyaan tidak akan dieksekusi" << endl;
+    }
+    
