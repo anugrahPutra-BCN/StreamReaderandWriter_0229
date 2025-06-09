beebd2f9694ae279bc099c116047b4d70cd1b223
AnugrahPutra_BCN <anugrahputra@gmail.com>
2025-06-09 19:10:21 +0700

    menambahkan catch

--- exceptionStandard.cpp
+++ exceptionStandard.cpp
@@ -13,4 +13,14 @@ int main()
         array<int, 3> data = {10, 20, 30};
         // pesan array integer 3 elemen
     }
-  
+    catch(exception &e)
+    {
+        // menangkap menggunakan obyek exception
+        cout << e.what() << endl;
+        /*akan dieksekusi karna array data hanya memiliki 3 elemen*/
+    }
+    cout << "Baris Program Yang terakhir" << endl;
+    /*penanda 2: bahwa program berjalan tnpa berhenti meskipun terjadi kesalahan*/
+    return 0;
+    
+}
